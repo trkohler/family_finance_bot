96ca7ed774eb9f31cbbc53b48826a1d08b4f4954
Troy Kohler <troy.kohler@zalando.de>
2022-07-04 23:52:38 +0200

    merge changes

--- app/handlers_connect.py
+++ app/handlers_connect.py
@@ -1,8 +1,8 @@
 from telegram.ext import MessageHandler, Filters
 
 from app.bot_handlers import how_many_remain, make_notion_card, show_all_type_of_spends
-from app.filters import FilterShowCategories, filter_remain, filter_spend
+from app.filters import filter_remain, filter_spend, filter_show_categories
 
 remain_handler = MessageHandler(filter_remain & Filters.chat_type.channel, how_many_remain)
 notion_handler = MessageHandler(filter_spend & Filters.chat_type.channel, make_notion_card)
-categories_handler = MessageHandler(FilterShowCategories & Filters.chat_type.channel, show_all_type_of_spends)
+categories_handler = MessageHandler(filter_show_categories & Filters.chat_type.channel, show_all_type_of_spends)
